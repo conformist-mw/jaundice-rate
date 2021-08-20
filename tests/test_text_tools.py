5f7aace9aa8c6f7e359e8757daa451b119eaa8f9
Oleg Smedyuk <conformist.mw@gmail.com>
2021-08-20 15:11:34 +0300

    move tests to separate module

---
+++ tests/test_text_tools.py
@@ -0,0 +1,28 @@
+import pymorphy2
+
+from text_tools import split_by_words, calculate_jaundice_rate
+
+
+def test_split_by_words():
+    # Экземпляры MorphAnalyzer занимают 10-15Мб RAM
+    # т.к. загружают в память много данных
+    # Старайтесь организовать свой код так, чтоб создавать экземпляр
+    # MorphAnalyzer заранее и в единственном числе
+    morph = pymorphy2.MorphAnalyzer()
+
+    assert split_by_words(
+        morph,
+        'Во-первых, он хочет, чтобы',
+    ) == ['во-первых', 'хотеть', 'чтобы']
+
+    assert split_by_words(
+        morph,
+        '«Удивительно, но это стало началом!»',
+    ) == ['удивительно', 'это', 'стать', 'начало']
+
+
+def test_calculate_jaundice_rate():
+    assert -0.01 < calculate_jaundice_rate([], []) < 0.01
+    assert 33.0 < calculate_jaundice_rate(
+        ['все', 'аутсайдер', 'побег'], ['аутсайдер', 'банкротство'],
+    ) < 34.0
--- text_tools.py
+++ text_tools.py
@@ -21,22 +21,7 @@ def split_by_words(morph, text):
     return words
 
 
-def test_split_by_words():
-    # Экземпляры MorphAnalyzer занимают 10-15Мб RAM
-    # т.к. загружают в память много данных
-    # Старайтесь организовать свой код так, чтоб создавать экземпляр
-    # MorphAnalyzer заранее и в единственном числе
-    morph = pymorphy2.MorphAnalyzer()
 
-    assert split_by_words(
-        morph,
-        'Во-первых, он хочет, чтобы',
-    ) == ['во-первых', 'хотеть', 'чтобы']
-
-    assert split_by_words(
-        morph,
-        '«Удивительно, но это стало началом!»',
-    ) == ['удивительно', 'это', 'стать', 'начало']
 
 
 def calculate_jaundice_rate(article_words, charged_words):
@@ -57,8 +42,3 @@ def calculate_jaundice_rate(article_words, charged_words):
     return round(score, 2)
 
 
-def test_calculate_jaundice_rate():
-    assert -0.01 < calculate_jaundice_rate([], []) < 0.01
-    assert 33.0 < calculate_jaundice_rate(
-        ['все', 'аутсайдер', 'побег'], ['аутсайдер', 'банкротство'],
-    ) < 34.0
